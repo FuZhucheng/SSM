017a36361db85497b7c5905c1022a52b00006631
FuZhucheng <751197996@qq.com>
2017-07-05 22:17:05 +0800

    【修改】修改部分

--- src/main/java/com/fuzhu/utils/JavaMailSendTest.java
+++ src/main/java/com/fuzhu/utils/JavaMailSendTest.java
@@ -18,8 +18,8 @@ public class JavaMailSendTest {
     // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
     //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-    public static String myEmailAccount = "751197996@qq.com";
-    public static String myEmailPassword = "comgtabdigwlbbdg";//这个就要填上我们刚刚拿到的授权码
+    public static String myEmailAccount = "@qq.com";
+    public static String myEmailPassword = "";//这个就要填上我们刚刚拿到的授权码
     // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
     // 网易163邮箱的 SMTP 服务器地址为: smtp.163.com；qq邮箱的SMTP服务器地址：smtp.qq.com
     public static String myEmailSMTPHost = "smtp.qq.com";
